44f4114fca78c5308198448988b520708f5b842e
davidpalacin <palacingimenezdavid.fpllefia@gmail.com>
2022-10-25 09:43:38 +0200

    ya me deja dropear

---
+++ main.js
@@ -0,0 +1,20 @@
+// Función que permite dropear, se inserta en el HTML como función en 'ondragover', se necesita hacer con preventDefault() para evitar el comportamiento por defecto.
+function allowDrop(ev){
+    ev.preventDefault();
+}
+
+function drag(ev){
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+let products = document.querySelectorAll('.fridge');
+  products.forEach(function(product) {
+    product.addEventListener('dragstart', allowDrop);
+    product.addEventListener('dragend', drop);
+});
